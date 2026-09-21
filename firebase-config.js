61679c09154d924c707088f2538a4c71102c3f2b
aaaksazzz <aaaksazzz1@gmail.com>
2026-09-21 10:02:20 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+
+apiKey: "PUT_YOUR_API_KEY",
+
+authDomain:
+"PUT_YOUR_PROJECT.firebaseapp.com",
+
+projectId:
+"PUT_YOUR_PROJECT_ID",
+
+storageBucket:
+"PUT_YOUR_STORAGE_BUCKET",
+
+messagingSenderId:
+"PUT_YOUR_MESSAGING_SENDER_ID",
+
+appId:
+"PUT_YOUR_APP_ID"
+
+};
